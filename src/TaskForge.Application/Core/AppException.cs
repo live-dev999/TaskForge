6dbbc00563cdb47a027192819f7e873964919a96
Dzianis Prokharchyk <live-dev@hotmail.com>
2025-10-31 21:08:00 +0300

    feat: add middleware for catch exceptions

---
+++ src/TaskForge.Application/Core/AppException.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskForge.Application.Core
+{
+    public class AppException
+    {
+        public AppException(int statusCode, string message, string details = null)
+        {
+            Details = details;
+            Message = message;
+            StatusCode = statusCode;
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Details { get; set; }
+    }
+}

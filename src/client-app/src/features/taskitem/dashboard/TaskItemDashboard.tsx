a2210560f7d95393691308a3132ee0b9c9b6840c
Dzianis Prokharchyk <live-dev@hotmail.com>
2025-10-31 19:23:55 +0300

    ref: code cleanup

--- src/client-app/src/features/taskitem/dashboard/TaskItemDashboard.tsx
+++ src/client-app/src/features/taskitem/dashboard/TaskItemDashboard.tsx
@@ -1,18 +1,16 @@
 import { observer } from "mobx-react-lite";
 import { Grid } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
-import TaskItemDetails from "../details/TaskItemDetails";
-import TaskItemForm from "../form/TaskItemForm";
 import TaskItemList from "./TaskItemList";
 import { useEffect } from "react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 export default observer(function TaskItemDashboard() {
     const { taskItemStore } = useStore();
-
+    const { loadTaskItems, taskItemRegistry } = taskItemStore;
     useEffect(() => {
-        taskItemStore.loadTaskItems();
-    }, [taskItemStore])
+        if (taskItemRegistry.size <= 1) loadTaskItems();
+    }, [loadTaskItems,taskItemRegistry])
 
     if (taskItemStore.loadingInitial)
         return (<LoadingComponent content='Loading app...' />)
@@ -20,7 +18,7 @@ export default observer(function TaskItemDashboard() {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <TaskItemList/>
+                <TaskItemList />
             </Grid.Column>
             <Grid.Column width='6'>
                 <h2>
--- src/client-app/src/features/taskitem/form/TaskItemForm.tsx
+++ src/client-app/src/features/taskitem/form/TaskItemForm.tsx
@@ -3,7 +3,7 @@ import { Button, Form, Segment } from "semantic-ui-react";
 import { TaskItem, TaskStatus } from "../../../app/models/taskItem";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 export default observer(function TaskItemForm() {
@@ -57,7 +57,7 @@ const navigate = useNavigate();
                 <Form.Input type="date" placeholder='Update Date' value={taskItem.updatedAt} name='updatedAt' onChange={handleInputChange} />
                 <Form.Input placeholder='Status' value={taskItem.status} name='status' onChange={handleInputChange} />
                  <Button loading={loading} floated='right' positive type='submit' content='Submit' />
-                <Button floated='right' positive type='button' content='Cancel' />
+                <Button floated='right' as={Link} to={'/taskItems'}  positive type='button' content='Cancel' />
             </Form>
         </Segment>
     )

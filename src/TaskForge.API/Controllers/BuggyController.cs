e4c94ea6c15dc7a2f7222768e8b2b9168157592f
Dzianis Prokharchyk <live-dev@hotmail.com>
2025-11-01 11:07:48 +0300

    feat: add controller for error testing

---
+++ src/TaskForge.API/Controllers/BuggyController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskForge.API.Controllers
+{
+
+    /// <summary>
+    /// Controller for testing client error behavior
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BuggyController : BaseApiController
+    {
+        #region Methods
+
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest("This is a bad request");
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            throw new Exception("This is a server error");
+        }
+
+        [HttpGet("unauthorised")]
+        public ActionResult GetUnauthorised()
+        {
+            return Unauthorized();
+        }
+
+        #endregion
+    }
+}
